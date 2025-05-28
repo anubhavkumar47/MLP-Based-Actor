49d14c2e0054f60802e671aad2d26176d2824f9e
Anubhav Kumar <151345311+anubhavkumar47@users.noreply.github.com>
2025-05-28 15:06:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ agent = TD3(
 replay_buffer = ReplayBuffer(state_dim, action_dim)
 
 # ==== Training settings ====
-episodes          = 10000
+episodes          = 300
 max_steps         = 150
 batch_size        = 512
 epsilon_start     = 1.0     # start fully random
@@ -41,11 +41,17 @@ aoi_list =[]
 train_call =0
 actor_losses=[]
 critic_losses=[]
+total_episode=[]
 
 # ==== Training loop ====
 for episode in range(episodes):
     state   = env.reset()
+    #print("State form en",state)
     ep_reward = 0
+    ep_energy =0
+    ep_aoi=0
+    ep_actor=0
+    ep_critic=0
 
     # linearly decay epsilon
     epsilon = max(
@@ -57,10 +63,10 @@ for episode in range(episodes):
         # ε-greedy action:
         if np.random.rand() < epsilon:
             action = env.action_space.sample()
-            print("Action from En ",action)
+            #print("Action from En ",action)
         else:
             action = agent.select_action(np.array(state))
-            print("Action from Po ",action)
+            #print("Action from Po ",action)
             # optional smoothing noise around the greedy action
             noise = np.random.normal(0, policy_noise, size=action_dim)
             action = (action + noise).clip(-max_action, max_action)
@@ -69,21 +75,26 @@ for episode in range(episodes):
         next_state, reward, done, energy,aoi = env.step(action)
         #print("Reward",step,reward)
         #print(energy)
-        energy_consumption.append(energy)
-        aoi_list.append(aoi)
-       # print(aoi)
+        #print(aoi)
 
         # store in buffer
         replay_buffer.add(state, action, reward, next_state, float(done))
 
         state      = next_state
         ep_reward += reward
+        ep_energy+=energy
+        ep_aoi+=aoi
+
 
         # train once buffer has enough samples
         if len(replay_buffer) >= batch_size:
             info_dict =agent.train(replay_buffer, batch_size)
-            critic_losses.append(info_dict["critic_loss"])
-            actor_losses.append(info_dict["actor_loss"])
+            ep_critic +=info_dict["critic_loss"]
+            if info_dict["actor_loss"] is None :
+                ep_actor +=0
+            else:
+                ep_actor+=info_dict["actor_loss"]
+
             #print(info_dict["actor_loss"],info_dict["critic_loss"])
             #train_call +=1
 
@@ -92,6 +103,11 @@ for episode in range(episodes):
             break
 
     episode_rewards.append(ep_reward / max_steps)
+    energy_consumption.append(ep_energy/max_steps)
+    critic_losses.append(ep_critic/max_steps)
+    actor_losses.append(ep_actor/max_steps)
+    aoi_list.append(ep_aoi)
+    total_episode.append(episode)
     print(f"Ep {episode+1:3d} | Avg Reward: {ep_reward/max_steps:8.2f} | ε = {epsilon:.3f}")
     print("--------------------------------------------------------------------------------------------------------------------")
 
@@ -99,14 +115,21 @@ for episode in range(episodes):
 #os.makedirs("checkpoints", exist_ok=True)
 #torch.save(agent.actor.state_dict(),  "checkpoints/actor.pth")
 #torch.save(agent.critic.state_dict(), "checkpoints/critic.pth")
+print("Episode",total_episode)
+print("Rewards",episode_rewards)
+print("Actor Loss",actor_losses)
+print("critic_losses",critic_losses)
+print("energy_consumption",energy_consumption)
+print("aoi_list",aoi_list)
 
 pd.DataFrame({
-    "Episode": list(range(1, episodes+1)),
+    "Episode": total_episode,
     "AvgReward": episode_rewards,
     "Actor Loss":actor_losses,
     "Critic Loss":critic_losses,
     "Energy":energy_consumption,
     "AoI":aoi_list
-}).to_csv("training_mlp_log.csv", index=False)
+
+}).to_csv("training_mlp_log_2.csv", index=False)
 
 print("Training complete. Logs in training_mlp_log.csv")
