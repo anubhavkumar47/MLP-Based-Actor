35a1433c197d330dfc3940a971750a7732cfac69
Anubhav Kumar <kumaranubhv47@gmail.com>
2025-08-18 19:31:30 +0530

    Update memory.py

--- memory.py
+++ memory.py
@@ -3,7 +3,7 @@ import torch
 
 
 class ReplayBuffer:
-    def __init__(self, state_dim, action_dim, max_size=int(1e4)):
+    def __init__(self, state_dim, action_dim, max_size=int(1e5)):
         self.max_size = max_size
         self.ptr = 0
         self.size = 0
@@ -38,3 +38,4 @@ class ReplayBuffer:
     def __len__(self):
 
         return self.size
+
