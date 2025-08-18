706304b30cafe7cc46f488478d372e3d99173060
Anubhav Kumar <kumaranubhv47@gmail.com>
2025-08-18 19:36:09 +0530

    Update policy.py

--- policy.py
+++ policy.py
@@ -11,8 +11,8 @@ class TD3:
         max_action,
         actor_class,
         critic_class,
-        actor_lr=1e-2,         # Lowered LR for stability
-        critic_lr=2e-2,
+        actor_lr=2e-3,         # Lowered LR for stability
+        critic_lr=2e-3,
         tau=0.005,
         gamma=0.99,
         policy_noise=0.2,
@@ -108,4 +108,5 @@ class TD3:
     def _soft_update(self, net, target_net):
         for param, target_param in zip(net.parameters(), target_net.parameters()):
             target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
 
